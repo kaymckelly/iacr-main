b87bb4aa56007ca4b90e801a722bce0379b68d4d
Kevin McCurley <github@digicrime.com>
2018-08-17 13:18:18 -0600

    Add a map of events

---
+++ scripts/addmap.js
@@ -0,0 +1,268 @@
+// This will be fetched via ajax from /events/query.php, but
+// for now it is hard-coded.
+var events =[{
+  "startdate": "2019-12-08",
+  "enddate": "2019-12-12",
+  "city": "Kobe",
+  "url": "http://",
+  "country": "Japan",
+  "submitdate": "0000-00-00",
+  "longname": "Asiacrypt 2019",
+  "lat": "34.666394",
+  "shortname": "Asiacrypt",
+  "lng": "135.214834"
+},
+             {
+               "startdate": "2019-08-18",
+               "enddate": "2019-08-22",
+               "city": "Santa Barbara",
+               "url": "http://www.iacr.org/conferences/",
+               "country": "USA",
+               "submitdate": "0000-00-00",
+               "longname": "Crypto 2019",
+               "lat": "34.4139629",
+               "shortname": "Crypto",
+               "lng": "-119.84894700000001"
+             },
+             {
+               "startdate": "2019-08-14",
+               "enddate": "2019-08-16",
+               "city": "Waterloo",
+               "url": "http://sacworkshop.org/",
+               "country": "Canada",
+               "submitdate": "0000-00-00",
+               "longname": "Selected Areas in Cryptography",
+               "lat": "43.25897255501914",
+               "shortname": "SAC 2019",
+               "lng": "-79.92074889472042"
+             },
+             {
+               "startdate": "2019-06-24",
+               "enddate": "2019-06-27",
+               "city": "Paris",
+               "url": "http://nutmic2019.imj-prg.fr/",
+               "country": "France",
+               "submitdate": "2019-02-28",
+               "longname": "Number-Theoretic Methods in Cryptology 2019 ",
+               "lat": "48.8471036",
+               "shortname": "NutMiC 2019",
+               "lng": "2.3574989999999616"
+             },
+             {
+               "startdate": "2019-06-04",
+               "enddate": "2019-06-07",
+               "city": "Svetlogorsk, Kaliningrad region",
+               "url": "http://www.ctcrypt.ru",
+               "country": "Russia",
+               "submitdate": "2019-02-18",
+               "longname": "8th Workshop on Current Trends in Cryptology",
+               "lat": "54.9272541",
+               "shortname": "CTCrypt'19",
+               "lng": "20.15794089999997"
+             },
+             {
+               "startdate": "2019-05-19",
+               "enddate": "2019-05-23",
+               "city": "Darmstadt",
+               "url": "http://eurocrypt.iacr.org/2019",
+               "country": "Germany",
+               "submitdate": "2018-10-04",
+               "longname": "Eurocrypt 2019",
+               "lat": "49.87432099999999",
+               "shortname": "Eurocrypt 2019",
+               "lng": "8.657713000000058"
+             },
+             {
+               "startdate": "2019-04-22",
+               "enddate": "2019-04-24",
+               "city": "Rabat",
+               "url": "http://www.c2si-conference.org/",
+               "country": "Morocco",
+               "submitdate": "0000-00-00",
+               "longname": "3rd International Conference on Codes, Cryptology and Information Security",
+               "lat": "34.0076598",
+               "shortname": "C2SI-2019",
+               "lng": "-6.8382596999999805"
+             },
+             {
+               "startdate": "2019-04-14",
+               "enddate": "2019-04-17",
+               "city": "Beijing",
+               "url": "http://pkc.iacr.org/2019",
+               "country": "China",
+               "submitdate": "2018-10-12",
+               "longname": "Public Key Cryptography",
+               "lat": "39.90419989999999",
+               "shortname": "PKC 2019",
+               "lng": "116.40739630000007"
+             },
+             {
+               "startdate": "2019-04-02",
+               "enddate": "2019-04-04",
+               "city": "Darmstadt",
+               "url": "http://www.cosade.org/",
+               "country": "Germany",
+               "submitdate": "2018-12-01",
+               "longname": "Workshop on Constructive Side-Channel Analysis and Secure Design",
+               "lat": "49.87432099999999",
+               "shortname": "COSADE '19",
+               "lng": "8.657713000000058"
+             },
+             {
+               "startdate": "2019-03-25",
+               "enddate": "2019-03-28",
+               "city": "Paris",
+               "url": "https://fse.iacr.org/2019/",
+               "country": "France",
+               "submitdate": "2018-11-23",
+               "longname": "Fast Software Encryption",
+               "lat": "48.8304658",
+               "shortname": "FSE 2019",
+               "lng": "2.3380701999999474"
+             },
+             {
+               "startdate": "2019-02-18",
+               "enddate": "2019-02-22",
+               "city": "Frigate Bay",
+               "url": "http://fc19.ifca.ai/",
+               "country": "St. Kitts",
+               "submitdate": "2018-09-18",
+               "longname": "Financial Cryptography and Data Security 2019",
+               "lat": "17.292751",
+               "shortname": "FC19",
+               "lng": "-62.685475999999994"
+             },
+             {
+               "startdate": "2019-01-09",
+               "enddate": "2019-01-11",
+               "city": "San Jose",
+               "url": "https://rwc.iacr.org/2019/",
+               "country": "USA",
+               "submitdate": "0000-00-00",
+               "longname": "Real World Crypto Symposium",
+               "lat": "37.3304071",
+               "shortname": "RWC 2019",
+               "lng": "-121.8882218"
+             },
+             {
+               "startdate": "2018-12-17",
+               "enddate": "2018-12-19",
+               "city": "Kanpur",
+               "url": "https://space2018.cse.iitk.ac.in/",
+               "country": "India",
+               "submitdate": "2018-07-20",
+               "longname": "Conference on Security, Privacy, and Applied Cryptography Engineering",
+               "lat": "26.5123388",
+               "shortname": "SPACE 2018",
+               "lng": "80.23289999999997"
+             },
+             {
+               "startdate": "2018-12-14",
+               "enddate": "2018-12-16",
+               "city": "Fuzhou",
+               "url": "http://xxhb.fjnu.edu.cn/inscrypt2018/",
+               "country": "China",
+               "submitdate": "2018-08-14",
+               "longname": "The 14th International Conference on Information Security and Cryptology",
+               "lat": "26.074478",
+               "shortname": "Inscrypt 2018",
+               "lng": "119.29648199999997"
+             },
+             {
+               "startdate": "2018-12-09",
+               "enddate": "2018-12-12",
+               "city": "New Delhi",
+               "url": "https://www.isical.ac.in/~indocrypt/",
+               "country": "India",
+               "submitdate": "2018-08-25",
+               "longname": "19th International Conference on Cryptology in India",
+               "lat": "28.6139391",
+               "shortname": "Indocrypt 2018",
+               "lng": "77.20902120000005"
+             },
+             {
+               "startdate": "2018-12-02",
+               "enddate": "2018-12-06",
+               "city": "Brisbane",
+               "url": "http://asiacrypt.iacr.org/2018/",
+               "country": "Australia",
+               "submitdate": "2018-05-11",
+               "longname": "Asiacrypt 2018",
+               "lat": "-27.4697707",
+               "shortname": "Asiacrypt",
+               "lng": "153.02512350000006"
+             },
+             {
+               "startdate": "2018-11-26",
+               "enddate": "2018-11-27",
+               "city": "Darmstadt",
+               "url": "http://ssr2018.net",
+               "country": "Germany",
+               "submitdate": "2018-06-22",
+               "longname": "4th Conference on Security Standards Research 2018",
+               "lat": "49.8728253",
+               "shortname": "SSR18",
+               "lng": "8.65119290000007"
+             },
+             {
+               "startdate": "2018-11-11",
+               "enddate": "2018-11-14",
+               "city": "Panaji, Goa",
+               "url": "https://tcc.iacr.org/2018",
+               "country": "India",
+               "submitdate": "2018-05-17",
+               "longname": "Theory of Cryptography Conference",
+               "lat": "15.456946",
+               "shortname": "TCC 2018",
+               "lng": "73.81336399999998"
+             },
+             {
+               "startdate": "2018-09-30",
+               "enddate": "2018-10-03",
+               "city": "Naples",
+               "url": "http://cans2018.na.icar.cnr.it",
+               "country": "Italy",
+               "submitdate": "2018-05-17",
+               "longname": "Cryptology and Network Security 2018",
+               "lat": "40.901975",
+               "shortname": "CANS 2018",
+               "lng": "14.33264399999996"
+             },
+             {
+               "startdate": "2018-09-20",
+               "enddate": "2018-09-21",
+               "city": "Iasi",
+               "url": "http://www.bcs2018.org/",
+               "country": "Romania",
+               "submitdate": "2018-05-27",
+               "longname": "Balkan CryptSec",
+               "lat": "47.1584549",
+               "shortname": "BCS 18",
+               "lng": "27.601441799999975"
+             }
+            ];
+
+
+// This displays a map and positions markers for events.
+function addMap() {
+  var centerlat = 0;
+  var centerlong = 0;
+  var map = L.map('map_canvas', {
+    center: [centerlat,centerlong],
+    zoom: 0});
+  var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  var osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+  var osm = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 18, attribution: osmAttrib});
+  map.addLayer(osm);
+  let group = [];
+  for (var i = 0; i < events.length; i++) {
+    let event = events[i];
+    let latlng = new L.LatLng(event.lat,event.lng,true);
+    let plotmark = new L.Marker(latlng);
+    plotmark.bindPopup('<h5><a href="' + event.url + '">' + event.longname + '</a></h5>' + event.city + ', ' + event.country + '<br>' + event.startdate + ' to ' + event.enddate);
+    group.push(plotmark);
+    map.addLayer(plotmark);
+  }
+  map.fitBounds(new L.featureGroup(group).getBounds());
+}
+window.addEventListener('DOMContentLoaded', addMap, false);
