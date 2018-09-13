f9b2028516718003e99c9ad64431cfb68928660f
Kay McKelly <frecklestars@gmail.com>
2018-09-13 15:00:12 -0700

    Added cardheadings and stuff to themes.css. Verified working. Still in process of switching over fresh apples theme to picker

--- kay3/scripts/themes.js
+++ kay3/scripts/themes.js
@@ -3,30 +3,36 @@ var current_theme = 'default';
 
 // The page loads with body class="default-body". You change the theme by removing default-body and changing to green-body (for whichever themes you set up in themes.css).
 function setTheme(new_theme) {
+  // body (whole page things)
   var body = document.getElementsByTagName('body')[0];
   body.classList.remove(current_theme + '-body');
   body.classList.add(new_theme + '-body');
 
+  // nav
   document.getElementById('menubar').classList.remove(current_theme + '-menubar');
   document.getElementById('menubar').classList.add(new_theme + '-menubar');
 
+  // dropdowns on nav
   var dropdowns = document.querySelectorAll('.' + current_theme + '-dropdown');
   for (var i = 0; i < dropdowns.length; i++) {
     dropdowns[i].classList.remove(current_theme + '-dropdown');
     dropdowns[i].classList.add(new_theme + '-dropdown');
   }
 
-  document.getElementById('main').classList.remove(current_theme + '-main');
-  document.getElementById('main').classList.add(new_theme + '-main');
+  // card headings
+  var cardHeadings = document.querySelectorAll('.' + current_theme + '-cardHeading');
+  for (var i = 0; i < cardHeadings.length; i++) {
+    cardHeadings[i].classList.remove(current_theme + '-cardHeading');
+    cardHeadings[i].classList.add(new_theme + '-cardHeading');
+  }
 
-  // var headers = document.querySelectorAll('.' + current_theme + '-card-header');
-  // for (i = 0; i < headers.length; ++i) {
-  //   headers[i].classList.remove(current_theme + '-card-header');
-  //   headers[i].classList.add(new_theme + '-card-header');
-  // }
+  // main tag
+  // document.getElementById('main').classList.remove(current_theme + '-main');
+  // document.getElementById('main').classList.add(new_theme + '-main');
 
-  document.getElementById('topbar').classList.remove(current_theme + '-topbar');
-  document.getElementById('topbar').classList.add(new_theme + '-topbar');
+  // header
+  // document.getElementById('topbar').classList.remove(current_theme + '-topbar');
+  // document.getElementById('topbar').classList.add(new_theme + '-topbar');
 
   current_theme = new_theme;
 }
