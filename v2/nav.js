c14dc865311d40f16606ed058897c4ec1c12e59f
Kevin McCurley <github@digicrime.com>
2018-11-03 12:02:56 -0600

    * alignment is better on first nav * ajax is now asynchronous (can't have <script> in injected html) * search forms work when you hit enter.

--- v2/nav.js
+++ v2/nav.js
@@ -1,4 +1,4 @@
-
+// This can be moved to headerFooter.js eventually.
 function isMobile() {
   return $('#iacrLogo').is(':hidden');
 }
@@ -17,3 +17,31 @@ $('#searchbox').blur(function() {
 });
                         
   
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+      isClosed = false;
+
+  trigger.click(function () {
+    hamburger_cross();
+  });
+
+  function hamburger_cross() {
+
+    if (isClosed == true) {
+      overlay.hide();
+      trigger.removeClass('is-open');
+      trigger.addClass('is-closed');
+      isClosed = false;
+    } else {
+      overlay.show();
+      trigger.removeClass('is-closed');
+      trigger.addClass('is-open');
+      isClosed = true;
+    }
+  }
+
+  $('[data-toggle="offcanvas"]').click(function () {
+    $('#wrapper').toggleClass('toggled');
+  });
+});
