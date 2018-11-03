3d5f9619ec0725156a976a4d1ff823251593c3e8
Kevin McCurley <github@digicrime.com>
2018-11-03 01:18:15 -0600

    Update to ID on header input. Also add a color on footer in cube theme.

--- v2/nav.js
+++ v2/nav.js
@@ -3,15 +3,17 @@ function isMobile() {
   return $('#iacrLogo').is(':hidden');
 }
 
-$('#searchInput2').focus(function() {
+$('#searchbox').focus(function() {
   if (isMobile()) return;
   $('#menuContainer').hide();
   $('#searchboxContainer').removeClass('w-25').addClass('w-100');
+  $('#searchInstructions').show();
 });
-$('#searchInput2').blur(function() {
+$('#searchbox').blur(function() {
   if (isMobile()) return;
   $('#menuContainer').show();
   $('#searchboxContainer').removeClass('w-100').addClass('w-25');
+  $('#searchInstructions').hide();
 });
                         
   
