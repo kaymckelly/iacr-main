8d7176512cdf0065fec9ff8f9e335b6dd58bc801
Kevin McCurley <github@digicrime.com>
2019-02-20 17:35:39 -0700

    Simplify javascript files to execute after header is loaded.

--- v4/headerFooter.js
+++ v4/headerFooter.js
@@ -1,9 +1,31 @@
+/* This function is to make sure that the menu collapses if you click
+   on the search button. It already appears that the search dropdown
+   will collapse if you touch the menu toggler.
+*/
+function addNavAnimations() {
+  $('#searchDropdownParent').on('show.bs.dropdown', function() {
+    $('#navbarSupportedContent').collapse('hide');
+    $('#hamburger').removeClass('active');
+  });
+  /* Animation for search dropdown menu. */
+  $('.dropdown').on('show.bs.dropdown', function(e) {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+    $('#searchbox').focus();
+  });
+
+  $('.dropdown').on('hide.bs.dropdown', function(e) {
+     $(this).find('.dropdown-menu').first().stop(true, true).slideUp(300);
+  });
+}
+
 $.ajax({
   url: 'header.html',
   dataType: 'html',
   cache: false,
   success: function(data) {
     $('#pageHeaderAndNav').html(data);
+    addNavAnimations();
+    $.getScript('search.js');
   },
   error: function(jqXHR, textStatus, errorThrow) {
     console.log(textStatus);
